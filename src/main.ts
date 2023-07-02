23ee747c3992ac3d2a8117f9b8df2dc956279bc3
Abdulkader El Rawas <63848905+abdulkaderelrawas@users.noreply.github.com>
2023-07-02 14:39:43 +0300

    swagger init

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app: NestExpressApplication = await NestFactory.create(AppModule);
@@ -10,6 +11,15 @@ async function bootstrap() {
   const port: number = config.get<number>('PORT');
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Url Shortener Task')
+    .setDescription('The Url Shortener API description')
+    .setVersion('1.0')
+    .addTag('url-shortener')
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('docs', app, document);
 
   await app.listen(port, () => {
     console.log('[WEB]', config.get<string>('BASE_URL'));
